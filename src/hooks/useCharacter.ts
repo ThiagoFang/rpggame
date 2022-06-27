8b3835c7cf0d8880e7bab5697885628c73e0b099
Thiago <thiagofang@gmail.com>
2022-06-27 14:10:44 -0300

    adicionando limitações de movimento ao jogador

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import { CharacterSides } from '../types/CharacterSides'
+import { CharacterSides } from '../types/CharacterSides';
+import { mapSpots } from '../data/mapSpots';
+import { useState } from 'react';
 
 type Position = {
     x: number,
@@ -7,19 +8,19 @@ type Position = {
 }
 
 export const useCharacter = () => {
-    const [pos, setPos] = useState<Position>({x: 2, y: 1});
+    const [pos, setPos] = useState<Position>({x: 2, y: 2});
     const [side, setSide] = useState<CharacterSides>('down');
 
     const moveLeft = () => {
         setPos(pos => ({
-            x: pos.x - 1,
+            x: canMove(pos.x -1, pos.y) ? pos.x -1 : pos.x,
             y: pos.y
         }));
         setSide('left');
     };
     const moveRight = () => {
         setPos(pos => ({
-            x: pos.x + 1,
+            x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
             y: pos.y
         }));
         setSide('right');
@@ -27,18 +28,27 @@ export const useCharacter = () => {
     const moveDown = () => {
         setPos(pos => ({
             x: pos.x,
-            y: pos.y + 1
+            y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y
         }));
         setSide('down');
     };
     const moveUp = () => {
         setPos(pos => ({
             x: pos.x,
-            y: pos.y - 1
+            y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y
         }));
         setSide('up');
     };
 
+    const canMove = (x: number, y: number) => {
+        if(mapSpots[y] !== undefined && mapSpots[y][x] !== undefined) {
+            if(mapSpots[y][x] === 1) {
+                return true
+            };
+        };
+
+        return false;
+    };
 
     return {
         x: pos.x,
